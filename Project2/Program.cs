7ca70967af4ebeebb6e64001c9e0c2d3efb61189
V.a6u <vki.vasu@gmail.com>
2024-01-15 14:31:23 +0000

    Added service for the seller

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Project2.EF;
+using Project2.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ISellerRepository, SellerRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
