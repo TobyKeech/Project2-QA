30152a42178c88cb4bab865b3403693050b2ade1
TobyKeech <toby.keech18@gmail.com>
2024-01-15 13:58:02 +0000

    added IpropertyService

---
+++ Project2/Business/DTO/PropertyDTO.cs
@@ -0,0 +1,32 @@
+﻿using Project2.Models;
+using Project2.Persistence.Repositories.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Business.DTO
+{
+    public class PropertyDTO : EntityBase, IEquatable<PropertyDTO>
+    {
+        public PropertyDTO()
+        {
+            // Bookings = new HashSet<BookingDTO>();
+        }
+        [Key]
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Postcode { get; set; }
+        public string Type { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public bool Garden { get; set; }
+        public decimal? Price { get; set; }
+        public string Status { get; set; }
+        public int SellerId { get; set; }
+        public int? BuyerId { get; set; }
+
+        public bool Equals(PropertyDTO? other)
+        {
+            return Id == other.Id;
+        }
+
+    }
+}
