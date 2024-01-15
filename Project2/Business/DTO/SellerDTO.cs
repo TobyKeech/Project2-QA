72c067744e826c95e76e693918776a17df50df9b
V.a6u <vki.vasu@gmail.com>
2024-01-15 15:00:23 +0000

    Create SellerDTO.cs

---
+++ Project2/Business/DTO/SellerDTO.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2.Models;
+using Project2.Persistence.Repositories.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Business.DTO
+{
+    public class SellerDTO : EntityBase, IEquatable<SellerDTO>
+    {
+        public SellerDTO() { }
+
+        [Key]
+        public override int Id { get; set; }
+        public int SellerId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Postcode { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+
+        public virtual ICollection<Property> Properties { get; set; }
+
+        public bool Equals(SellerDTO? other)
+        {
+            return Id == other.Id;
+        }
+
+        public object Clone()
+        {
+            return new SellerDTO
+            {
+                Id = this.Id,
+                SellerId = this.SellerId,
+                FirstName = this.FirstName,
+                Surname = this.Surname,
+                Address = this.Address,
+                Postcode = this.Postcode,
+                Phone = this.Phone,
+                Properties = this.Properties
+            };
+        }
+    }
+}
