6bf88214ebcfad425ef22dd2ca74bf0952d8b5ea
V.a6u <vki.vasu@gmail.com>
2024-01-15 15:02:27 +0000

    Added mapper for seller

--- Project2/Business/Services/TPCAutoMapper.cs
+++ Project2/Business/Services/TPCAutoMapper.cs
@@ -1,6 +1,16 @@
-﻿namespace Project2.Business.Services
+﻿using AutoMapper;
+using Project2.Business.DTO;
+using Project2.Models;
+
+namespace Project2.Business.Services
 {
-    public class TPCAutoMapper
+    public class TPCAutoMapper : Profile
     {
+        public TPCAutoMapper()
+        {
+            //Mapping for Seller
+            CreateMap<Seller, SellerDTO>();
+            CreateMap<SellerDTO, Seller>();
+        }
     }
 }
