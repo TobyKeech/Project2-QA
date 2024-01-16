23b1bf6291a04116928e1314087b5b371f62e1c6
V.a6u <vki.vasu@gmail.com>
2024-01-16 14:32:41 +0000

    Update SellerControllerTests.cs

--- Project2Test/SellerControllerTests.cs
+++ Project2Test/SellerControllerTests.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Project2.Business.DTO;
+using Project2.Business.Services;
+using Project2.Controllers;
+using Project2.EF;
+using Project2.Models;
+using Project2.Persistence.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +17,40 @@ namespace Project2Test
 {
     public class SellerControllerTests
     {
+
+        private Mapper _mapper;
+
+        public SellerControllerTests()
+        {
+            TPCAutoMapper myProfile = new TPCAutoMapper();
+            MapperConfiguration configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            _mapper = new Mapper(configuration);
+        }
+
+
+        private IServiceProvider GetBuyerServiceProivder()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddDbContext<EstateContext>(options => options.UseInMemoryDatabase("MockDBData"));
+            services.AddScoped<IBuyerService, BuyerService>();
+            services.AddScoped<IBuyerRepository, BuyerRepository>();
+            services.AddScoped<BuyerController>();
+            services.AddAutoMapper(typeof(Program));
+            services.AddControllers();
+            return services.BuildServiceProvider();
+        }
+
+        private BuyerDTO GetMockBuyer()
+        {
+            return new BuyerDTO
+            {
+                FirstName = "Steve",
+                Surname = "Smith",
+                Address = "1 Main Street",
+                Postcode = "TE 5T",
+                Phone = "12345"
+            };
+        }
     }
 }
