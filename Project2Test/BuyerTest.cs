e5067eaa9949c22ff1f10a21c615d65fc596c60d
Gemmag7 <72978574+Gemmag7@users.noreply.github.com>
2024-01-16 14:56:34 +0000

    tests working?

--- Project2Test/BuyerTest.cs
+++ Project2Test/BuyerTest.cs
@@ -7,34 +7,36 @@ using Project2.Business.Services;
 using Project2.Controllers;
 using Project2.EF;
 using Project2.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Project2.Models;
+using System.Linq;
+using System;
 
 namespace Project2Test
 {
-    public class BuyerTest
+    public class BuyerUnitTests
     {
         private Mapper _mapper;
 
-
-        public BuyerTest()
+        public BuyerUnitTests()
         {
             TPCAutoMapper myProfile = new TPCAutoMapper();
             MapperConfiguration configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
             _mapper = new Mapper(configuration);
         }
-    
 
-    private IServiceProvider GetBuyerServiceProivder()
-    {
-        ServiceCollection services = new ServiceCollection();
+        private IServiceProvider GetBuyerServiceProivder()
+        {
+            ServiceCollection services = new ServiceCollection();
 
-        services.AddDbContext<EstateContext>(options => options.UseInMemoryDatabase("MockDBData"));
-        services.AddScoped<IBuyerService, BuyerService>();
-        services.AddScoped<IBuyerRepository, BuyerRepository>();
-        services.AddScoped<BuyerController>();
-        services.AddAutoMapper(typeof(Program));
-        services.AddControllers();
-        return services.BuildServiceProvider();
-    }
+            services.AddDbContext<EstateContext>(options => options.UseInMemoryDatabase("MockDBData"));
+            services.AddScoped<IBuyerService, BuyerService>();
+            services.AddScoped<IBuyerRepository, BuyerRepository>();
+            services.AddScoped<BuyerController>();
+            services.AddAutoMapper(typeof(Program));
+            services.AddControllers();
+            return services.BuildServiceProvider();
+        }
 
         private BuyerDTO GetMockBuyer()
         {
@@ -46,6 +48,151 @@ namespace Project2Test
                 Postcode = "TE 5T",
                 Phone = "12345"
             };
+        }
+
+        [Fact]
+        public void TestAddBuyer()
+        {
+            var services = GetBuyerServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBuyerRepository>();
+                var service = new BuyerService(repo, _mapper);
+                var context = scope.ServiceProvider.GetService<EstateContext>();
+                var controller = new BuyerController(service);
+                //Clear database
+                context.Database.EnsureDeleted();
+
+                var buyerDTO = new BuyerDTO
+                {
+                    FirstName = "testname",
+                    Surname = "surnametest",
+                    Address = "123 test street",
+                    Postcode = "TE 5T",
+                    Phone = "123456789"
+                };
+
+                controller.AddBuyer(buyerDTO);
+                var buyer = context.Buyers.Single();
+
+                Assert.Equal(1, buyer.Id);
+                Assert.Equal("testname", buyer.FirstName);
+            }
+        }
+        [Fact]
+ 
+        public void TestGetBuyerById()
+        {
+            var services = GetBuyerServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBuyerRepository>();
+                var service = new BuyerService(repo, _mapper);
+                var context = scope.ServiceProvider.GetService<EstateContext>();
+                var controller = new BuyerController(service);
+                //Clear database
+                context.Database.EnsureDeleted();
+
+                var BuyerDTO = new BuyerDTO
+                {
+                    FirstName = "David",
+                    Surname = "Williams",
+                    Address = "100 Magnor Road, Newport",
+                    Postcode = "NP1 2LL 8RR",
+                    Phone = "01234567891",
+                   
+                };
+
+                controller.AddBuyer(BuyerDTO);
+                var buyer = context.Buyers.Single();
+
+                Assert.Equal(1, buyer.Id);
+                Assert.Equal("100 Magnor Road, Newport", buyer.Address);
+            }
+        }
+        [Fact]
+        public void TestGetBuyer()
+        {
+            var services = GetBuyerServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBuyerRepository>();
+                var service = new BuyerService(repo, _mapper);
+                var context = scope.ServiceProvider.GetService<EstateContext>();
+                var controller = new BuyerController(service);
+                //Clear database
+                context.Database.EnsureDeleted();
+
+                controller.AddBuyer(GetMockBuyer());
+
+
+
+                Assert.Equal(1, context.Buyers.Count());
+                Assert.Equal("Steve", context.Buyers.FirstOrDefault().FirstName);
+            }
+        }
+
+        [Fact]
+        public void UpdateBuyer()
+        {
+            var services = GetBuyerServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBuyerRepository>();
+                var service = new BuyerService(repo, _mapper);
+                var context = scope.ServiceProvider.GetService<EstateContext>();
+                var controller = new BuyerController(service);
+                //Clear database
+                context.Database.EnsureDeleted();
+
+                var BuyerDTO = new BuyerDTO
+                {
+                    FirstName = "David",
+                    Surname = "Williams",
+                    Address = "100 Magnor Road, Newport",
+                    Postcode = "NP1 2LL V8RR",
+                    Phone = "01234567891",
+
+                };
+
+                controller.AddBuyer(BuyerDTO);
+                
+                var buyer = context.Buyers.Single();
+               
+                Assert.Equal(1, buyer.Id);
+                Assert.Equal("100 Magnor Road, Newport", buyer.Address);
+            }
+        }
+
+        [Fact]
+        public void TestDeleteBuyer()
+        {
+            var services = GetBuyerServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBuyerRepository>();
+                var service = new BuyerService(repo, _mapper);
+                var context = scope.ServiceProvider.GetService<EstateContext>();
+                var controller = new BuyerController(service);
+                //Clear database
+                context.Database.EnsureDeleted();
+
+                var BuyerDTO = new BuyerDTO
+                {
+                    FirstName = "David",
+                    Surname = "Williams",
+                    Address = "100 Magnor Road, Newport",
+                    Postcode = "NP1 2LL V8RR",
+                    Phone = "01234567891",
+
+                };
+
+                controller.AddBuyer(BuyerDTO);
+                var buyer = context.Buyers.Single();
+
+                Assert.Equal(1, buyer.Id);
+                Assert.Equal("100 Magnor Road, Newport", buyer.Address);
+            }
         }
     }
 }
