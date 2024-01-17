b42d1eef41ad0ed5087c901e3a521fcf3476925a
TobyKeech <toby.keech18@gmail.com>
2024-01-17 10:24:53 +0000

    changes to the controller and cors bypass added

--- Project2/Controllers/PropertyController.cs
+++ Project2/Controllers/PropertyController.cs
@@ -6,7 +6,7 @@ using System.Net;
 
 namespace Project2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PropertyController : ControllerBase
     {
--- Project2/Program.cs
+++ Project2/Program.cs
@@ -3,7 +3,21 @@ using Project2.Business.Services;
 using Project2.EF;
 using Project2.Persistence.Repositories;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; //Needed for Cors
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            //policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 // Add services to the container.
 builder.Services.AddScoped<IBuyerService, BuyerService>();
 builder.Services.AddScoped<IBuyerRepository, BuyerRepository>();
@@ -13,6 +27,8 @@ builder.Services.AddScoped<IPropertyService, PropertyService>();
 builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
 
 builder.Services.AddAutoMapper(typeof(Program));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +42,8 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,3 +59,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
